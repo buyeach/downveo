5699dc023555f1b86f5c94ef481786fa3839c1dc
buyeach <152355447+buyeach@users.noreply.github.com>
2025-12-13 03:52:26 +0800

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,2 +1,13 @@
 import { handler as fetch } from "../serve.ts";
-export default fetch;
+
+export default async function handler(request: Request): Promise<Response> {
+    const url = new URL(request.url);
+
+    if (url.pathname === '/' && !url.searchParams.has('url')) {
+        const exampleUrl = 'https://v.douyin.com/n_r5jmCP31I';
+        const redirectUrl = `https://down.aibyai.cn/?url=${encodeURIComponent(exampleUrl)}&data=true`;
+        return Response.redirect(redirectUrl, 302);
+    }
+
+    return fetch(request);
+}
